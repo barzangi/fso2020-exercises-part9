a3ff2996ca9b966b63e51df3a471329b098da79c
Layth Barzangi <thebarzangi@gmail.com>
2020-03-21 21:47:52 +0400

    make bmi calculator code a separate typescript module

---
+++ healthcalc/modules/bmiCalc.ts
@@ -0,0 +1,14 @@
+const calculateBmi = (height: number, weight: number): String => {
+  const bmi = weight / Math.pow(height / 100, 2)
+  if (bmi < 18.5) {
+    return 'Underweight (unhealthy weight)'
+  } else if (bmi >= 18.5 && bmi <= 25) {
+    return 'Normal (healthy weight)'
+  } else if (bmi >= 25 && bmi <= 30) {
+    return 'Overweight (unhealthy weight)'
+  } else {
+    return 'Obese (unhealthy weight)'
+  }
+}
+
+export default calculateBmi
