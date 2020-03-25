d3a13545437e187e166db5afa904cca3b0d9e0d1
Layth Barzangi <thebarzangi@gmail.com>
2020-03-25 23:39:42 +0400

    added diagnoses and patients routes

---
+++ patientor/backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import diagnosesRouter from './routes/diagnoses'
+import patientsRouter from './routes/patients'
+const app = express()
+app.use(express.json())
+
+import cors from 'cors'
+app.use(cors())
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here')
+  res.send('pong')
+})
+
+app.use('/api/diagnoses', diagnosesRouter)
+app.use('/api/patients', patientsRouter)
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
