f6168776d50dcc33b60b83fe3f5292820b095e83
Layth Barzangi <thebarzangi@gmail.com>
2020-03-27 23:36:29 +0400

    added type for new patient entry, refactored gender field as enum

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -4,13 +4,21 @@ export interface DiagnosesEntry {
   latin?: string;
 }
 
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other'
+}
+
 export interface PatientEntry {
   id: string;
   name: string;
   dateOfBirth: string;
   ssn: string;
-  gender: string;
+  gender: Gender;
   occupation: string;
 }
 
 export type NonSsnPatientEntry = Omit<PatientEntry, 'ssn'>
+
+export type NewPatientEntry = Omit<PatientEntry, 'id'>
