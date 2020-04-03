82a1f2b0b1dc60f428b9bed9b0c4a95546db7945
Layth Barzangi <thebarzangi@gmail.com>
2020-04-04 00:07:51 +0400

    #BUG: Does not compile! (TypeError: props.forEach is not a function)

--- courseinfo-ts/src/index.tsx
+++ courseinfo-ts/src/index.tsx
@@ -5,27 +5,59 @@ interface HeaderProps {
   name: string;
 }
 
-interface CoursePartProps {
-  name: string;
-  exerciseCount: number;
-}
-
-interface TotalProps {
-  total: number;
-}
-
 const Header: React.FC<HeaderProps> = (props) => {
   return <h1>{props.name}</h1>
 }
 
-const Content: React.FC<CoursePartProps[]> = (props) => {
-  return (
-    <div>
-      <p>{props[0].name} {props[0].exerciseCount}</p>
-      <p>{props[1].name} {props[1].exerciseCount}</p>
-      <p>{props[2].name} {props[2].exerciseCount}</p>
-    </div>  
-  )
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartWithDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends CoursePartWithDescription {
+  name: 'Fundamentals';
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: 'Using props to pass data';
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartWithDescription {
+  name: 'Deeper type usage';
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Content: React.FC<CoursePart[]> = (props): any => {
+
+  // Helper function for exhaustive type checking
+  const assertNever = (value: never): never => {
+    throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`)
+  }
+  // Does not compile! (TypeError: props.forEach is not a function)
+  props.forEach(part => {
+    switch (part.name) {
+      case 'Fundamentals':
+        return <p>{part.name} {part.exerciseCount} {part.description}</p>
+      case 'Using props to pass data':
+        return <p>{part.name} {part.exerciseCount} {part.groupProjectCount}</p>
+      case 'Deeper type usage':
+        return <p>{part.name} {part.exerciseCount} {part.description} {part.exerciseSubmissionLink}</p>
+      default:
+        return assertNever(part)
+    }
+  })
+}
+
+interface TotalProps {
+  total: number;
 }
 
 const Total: React.FC<TotalProps> = (props) => {
@@ -34,18 +66,22 @@ const Total: React.FC<TotalProps> = (props) => {
 
 const App: React.FC = () => {
   const courseName = 'Half Stack Application Development'
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: 'This is an awesome course part'
     },
     {
       name: 'Using props to pass data',
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: 'Deeper type usage',
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: 'Confusing description',
+      exerciseSubmissionLink: 'https://fake.made-up-url.dev'
     }
   ]
 
