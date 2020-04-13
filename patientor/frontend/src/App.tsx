90bcf14b524f2b23cbe2e3879f218c3e45b8361b
Layth Barzangi <thebarzangi@gmail.com>
2020-04-13 20:36:58 +0400

    added SinglePatientPage component to router

--- patientor/frontend/src/App.tsx
+++ patientor/frontend/src/App.tsx
@@ -8,6 +8,7 @@ import { useStateValue } from "./state";
 import { Patient } from "./types";
 
 import PatientListPage from "./PatientListPage";
+import SinglePatientPage from './SinglePatientPage'
 
 const App: React.FC = () => {
   const [, dispatch] = useStateValue();
@@ -37,7 +38,8 @@ const App: React.FC = () => {
           </Button>
           <Divider hidden />
           <Switch>
-            <Route path="/" render={() => <PatientListPage />} />
+            <Route exact path="/" render={() => <PatientListPage />} />
+            <Route path='/patients/:id' render={() => <SinglePatientPage />} />
           </Switch>
         </Container>
       </Router>
