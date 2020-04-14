9f22544a57d9f0b525be62f06057ff45ecb0cc00
Layth Barzangi <thebarzangi@gmail.com>
2020-04-14 18:54:07 +0400

    expanded type definitions for patient 'entries' field, combining three healthcare subtypes

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -1,4 +1,4 @@
-export interface DiagnosesEntry {
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
@@ -10,9 +10,44 @@ export enum Gender {
   Other = 'other'
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'Low Risk' = 1,
+  'High Risk' = 2,
+  'Critical Risk' = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry
 
 export interface Patient {
   id: string;
