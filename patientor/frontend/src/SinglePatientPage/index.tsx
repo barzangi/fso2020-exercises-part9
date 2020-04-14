bc0dbe765c79fa5692b451aae9ff177d87677f9c
Layth Barzangi <thebarzangi@gmail.com>
2020-04-14 19:14:22 +0400

    expanded patient page to display healthcare entries and diagnosis codes with their descriptions

--- patientor/frontend/src/SinglePatientPage/index.tsx
+++ patientor/frontend/src/SinglePatientPage/index.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import { Icon } from 'semantic-ui-react'
-import { Patient } from "../types"
+import { Patient, Diagnosis } from "../types"
 import { apiBaseUrl } from "../constants"
 import { useStateValue, setSinglePatient } from '../state'
 
 const SinglePatientPage: React.FC = () => {
-  const [{ singlePatient }, dispatch] = useStateValue()
+  const [{ singlePatient, diagnoses }, dispatch] = useStateValue()
 
   const { id } = useParams<{ id: string }>()
 
@@ -16,7 +16,7 @@ const SinglePatientPage: React.FC = () => {
       const { data: singlePatientFromApi } = await axios.get<Patient>(
         `${apiBaseUrl}/patients/${id}`
       )
-      dispatch(setSinglePatient(singlePatientFromApi));
+      dispatch(setSinglePatient(singlePatientFromApi))
     } catch (e) {
       console.error(e)
     }
@@ -31,6 +31,29 @@ const SinglePatientPage: React.FC = () => {
       <div>Date of Birth: {singlePatient.dateOfBirth}</div>
       <div>SSN: {singlePatient.ssn}</div>
       <div>Occupation: {singlePatient.occupation}</div>
+      {singlePatient.entries.length === 0
+      ? <h2>No entries</h2>
+      :
+      <>
+        <h2>Entries</h2>
+        {singlePatient.entries.map(entry =>
+          <>
+            <div>{entry.date} : {entry.description}</div>
+            {!entry.diagnosisCodes
+            ? null
+            :
+            <ul>
+              {entry.diagnosisCodes?.map(dg =>
+                <li>
+                  {dg} - {Object.values(diagnoses).filter(d => d.code === dg)[0].name}
+                </li>  
+              )}
+            </ul>
+            }
+          </>
+        )}
+      </>
+      }
     </div>
   )
 }
