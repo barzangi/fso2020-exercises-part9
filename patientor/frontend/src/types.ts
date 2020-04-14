dd9537e79e150bebc275f8e78180b596af647518
Layth Barzangi <thebarzangi@gmail.com>
2020-04-14 18:58:54 +0400

    updated type definitions to correspond to extended types on backend

--- patientor/frontend/src/types.ts
+++ patientor/frontend/src/types.ts
@@ -10,6 +10,45 @@ export enum Gender {
   Other = "other"
 }
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'Low Risk' = 1,
+  'High Risk' = 2,
+  'Critical Risk' = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,4 +56,5 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
