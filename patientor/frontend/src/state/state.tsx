1906bbdfd8320035c07aae33c661a76233640340
Layth Barzangi <thebarzangi@gmail.com>
2020-04-14 19:10:32 +0400

    added diagnoses property to state type definition

--- patientor/frontend/src/state/state.tsx
+++ patientor/frontend/src/state/state.tsx
@@ -1,16 +1,18 @@
 import React, { createContext, useContext, useReducer } from "react";
-import { Patient } from "../types";
+import { Patient, Diagnosis } from "../types";
 
 import { Action } from "./reducer";
 
 export type State = {
     patients: { [id: string]: Patient };
     singlePatient: Patient | undefined;
+    diagnoses: { [code: string]: Diagnosis}
   }
 
 const initialState: State = {
   patients: {},
-  singlePatient: undefined
+  singlePatient: undefined,
+  diagnoses: {}
 };
 
 export const StateContext = createContext<[State, React.Dispatch<Action>]>([
