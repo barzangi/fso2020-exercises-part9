89363abcd746a00154a7a9f82926237433084fe2
Layth Barzangi <thebarzangi@gmail.com>
2020-04-16 21:59:57 +0400

    use function with switch statement instead of in-line conditional operator to render gender icon

--- patientor/frontend/src/SinglePatientPage/index.tsx
+++ patientor/frontend/src/SinglePatientPage/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import { Icon } from 'semantic-ui-react'
-import { Patient, Diagnosis } from "../types"
+import { Patient, Gender } from "../types"
 import { apiBaseUrl } from "../constants"
 import { useStateValue, setSinglePatient } from '../state'
 
@@ -25,13 +25,26 @@ const SinglePatientPage: React.FC = () => {
   if (!singlePatient || id !== singlePatient.id) getSinglePatient(id)
 
   if (!singlePatient) return null
+
+  const genderIcon = (gender: Gender) => {
+    switch (gender) {
+      case 'male':
+        return <Icon name='mars' />
+      case 'female':
+        return <Icon name='venus' />
+      case 'other':
+        return <Icon name='genderless' />
+      default:
+        return null
+    }
+  }
   return (
     <div className='App'>
-      <h2>{singlePatient.name} {singlePatient.gender === 'male' ? <Icon name='mars' /> : <Icon name='venus' />}</h2>
+      <h2>{singlePatient.name} {genderIcon(singlePatient.gender)}</h2>
       <div>Date of Birth: {singlePatient.dateOfBirth}</div>
       <div>SSN: {singlePatient.ssn}</div>
       <div>Occupation: {singlePatient.occupation}</div>
-      {singlePatient.entries.length === 0
+      {!singlePatient.entries || singlePatient.entries.length === 0
       ? <h2>No entries</h2>
       :
       <>
