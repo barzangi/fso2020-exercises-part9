0da8451ebb83d4d783506904a10d0e6d915a2161
Layth Barzangi <thebarzangi@gmail.com>
2020-04-17 20:59:00 +0400

    updated page with extended entry listing component (EntryDetails) to show full info per entry

--- patientor/frontend/src/SinglePatientPage/index.tsx
+++ patientor/frontend/src/SinglePatientPage/index.tsx
@@ -2,12 +2,123 @@ import React from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import { Icon } from 'semantic-ui-react'
-import { Patient, Gender } from "../types"
+import { Patient, Gender, Entry, HealthCheckRating } from "../types"
 import { apiBaseUrl } from "../constants"
 import { useStateValue, setSinglePatient } from '../state'
 
+const ListDiagnoses: React.FC<{ entry: Entry }> = ({ entry }) => {
+  const [{ diagnoses }] = useStateValue()
+  return (
+    <>
+      {!entry.diagnosisCodes
+      ? null
+      :
+      <ul>
+        {entry.diagnosisCodes?.map(dg =>
+          <li key={dg}>
+            {dg} - {Object.values(diagnoses).filter(d => d.code === dg)[0].name}
+          </li>  
+        )}
+      </ul>
+      }
+    </>
+  )
+}
+
+const HospitalEntry: React.FC<{ entry: Entry }> = ({ entry }) => {
+  const thisDischarge = 'discharge' in entry ? entry.discharge : null
+  return (
+    <>
+      <h3>{entry.date} <Icon name='hospital' /></h3>
+      <div>Specialist: {entry.specialist}</div>
+      <h4>{entry.description}</h4>
+      <ListDiagnoses entry={entry} />
+      {!thisDischarge
+      ? null
+      :
+      <>
+        <div><strong>Discharge</strong></div>
+        <div>Date: {thisDischarge.date}</div>
+        <div>Criteria: {thisDischarge.criteria}</div>
+      </>
+      }
+    </>
+  )
+}
+
+const OccupationalhealthcareEntry: React.FC<{ entry: Entry }> = ({ entry }) => {
+  const thisEmployerName = 'employerName' in entry ? entry.employerName : null
+  const thisSickLeave = 'sickLeave' in entry ? entry.sickLeave : null
+  return (
+    <>
+      <h3>{entry.date} <Icon name='stethoscope' /> {thisEmployerName}</h3>
+      <div>Specialist: {entry.specialist}</div>
+      <h4>{entry.description}</h4>
+      <ListDiagnoses entry={entry} />
+      {!thisSickLeave
+      ? null
+      :
+      <>
+        <div><strong>Sick Leave</strong></div>
+        <div>Start Date: {thisSickLeave.startDate}</div>
+        <div>End Date: {thisSickLeave.endDate}</div>
+      </>
+      }
+    </>
+  )
+}
+
+const HealthCheckEntry: React.FC<{ entry: Entry }> = ({ entry }) => {
+  const thisHealthCheckRating = 'healthCheckRating' in entry ? entry.healthCheckRating : null
+  
+  const healthCheckRatingIcon = (healthCheckRating: HealthCheckRating) => {
+    switch (healthCheckRating) {
+      case 0:
+        return <p><Icon name='heart' color='green' /></p>
+      case 1:
+        return <p><Icon name='heart' color='yellow' /></p>
+      case 2:
+       return <p><Icon name='heart' color='orange' /></p>
+      case 3:
+        return <p><Icon name='heart' color='red' /></p>
+      default:
+        return null
+    }
+  }
+  return (
+    <>
+      <h3>{entry.date} <Icon name='user md' /></h3>
+      <div>Specialist: {entry.specialist}</div>
+      <h4>{entry.description}</h4>
+      <ListDiagnoses entry={entry} />
+      {thisHealthCheckRating === null
+      ? null
+      : healthCheckRatingIcon(thisHealthCheckRating)
+      }
+    </>
+  )
+}
+
+const EntryDetails: React.FC<{ entry: Entry }> = ({ entry }) => {
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value )}`
+    )
+  }
+  switch (entry.type) {
+    case 'Hospital':
+      return <HospitalEntry entry={entry} />
+    case 'OccupationalHealthcare':
+      return <OccupationalhealthcareEntry entry={entry} />
+    case 'HealthCheck':
+      return <HealthCheckEntry entry={entry} />
+    default:
+      return assertNever(entry)
+  }
+}
+
 const SinglePatientPage: React.FC = () => {
-  const [{ singlePatient, diagnoses }, dispatch] = useStateValue()
+  const [{ singlePatient }, dispatch] = useStateValue()
 
   const { id } = useParams<{ id: string }>()
 
@@ -50,20 +161,9 @@ const SinglePatientPage: React.FC = () => {
       <>
         <h2>Entries</h2>
         {singlePatient.entries.map(entry =>
-          <>
-            <div>{entry.date} : {entry.description}</div>
-            {!entry.diagnosisCodes
-            ? null
-            :
-            <ul>
-              {entry.diagnosisCodes?.map(dg =>
-                <li>
-                  {dg} - {Object.values(diagnoses).filter(d => d.code === dg)[0].name}
-                </li>  
-              )}
-            </ul>
-            }
-          </>
+          <div className='entryStyle'>
+            <EntryDetails key={entry.id} entry={entry} />
+          </div>
         )}
       </>
       }
