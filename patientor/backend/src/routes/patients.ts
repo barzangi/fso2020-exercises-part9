4caee014fe3f3d44ecdd77c68a50fa7aead0f16e
Layth Barzangi <thebarzangi@gmail.com>
2020-04-29 22:22:26 +0400

    added route to add new healthcare entry to existing patient

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -13,8 +13,8 @@ router.get('/', (_req, res) => {
 router.post('/', (req, res) => {
   try {
     const newPatientEntry = toNewPatientEntry(req.body)
-    const addedEntry = patientService.addPatientEntry(newPatientEntry)
-    res.json(addedEntry)
+    const addedPatientEntry = patientService.addPatientEntry(newPatientEntry)
+    res.json(addedPatientEntry)
   } catch (e) {
     res.status(400).send(e.message)
   }
@@ -25,6 +25,16 @@ router.get('/:id', (req, res) => {
   try {
     const patient = patientService.getPatient(req.params.id)
     res.json(patient)
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
+})
+
+// add healthcare entry to patient
+router.post('/:id/entries', (req, res) => {
+  try {
+    const addedHealthcareEntry = patientService.addHealthcareEntry(req.params.id, req.body)
+    res.json(addedHealthcareEntry)
   } catch (e) {
     res.status(400).send(e.message)
   }
