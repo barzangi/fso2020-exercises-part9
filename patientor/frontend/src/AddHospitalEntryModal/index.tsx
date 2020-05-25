caf30e8c2e6a182c6e85656f39bc6e3f001f9b53
Layth Barzangi <thebarzangi@gmail.com>
2020-05-25 16:30:39 +0400

    Modal to add healthcare entry of type "Hospital"

---
+++ patientor/frontend/src/AddHospitalEntryModal/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Modal, Segment } from 'semantic-ui-react'
+import AddHospitalEntryForm, { HospitalEntryFormValues } from './AddHospitalEntryForm'
+
+interface Props {
+  modalOpen: boolean;
+  onClose: () => void;
+  onSubmit: (values: HospitalEntryFormValues) => void;
+  error?: string;
+  patientName: string;
+}
+
+const AddHospitalEntryModal = ({ modalOpen, onClose, onSubmit, error, patientName }: Props) => (
+  <Modal open={modalOpen} onClose={onClose} centered={false} closeIcon>
+    <Modal.Header>Add Hospital entry for patient: {patientName}</Modal.Header>
+    <Modal.Content>
+      {error && <Segment inverted color="red">{`Error: ${error}`}</Segment>}
+      <AddHospitalEntryForm onSubmit={onSubmit} onCancel={onClose} />
+    </Modal.Content>
+  </Modal>
+)
+
+export default AddHospitalEntryModal
