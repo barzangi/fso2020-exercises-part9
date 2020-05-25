d5dd646ffd811b88a5d33e9ee97cf3951c6adadb
Layth Barzangi <thebarzangi@gmail.com>
2020-05-25 16:25:15 +0400

    Modal to add healthcare entry of type "HealthCheck"

---
+++ patientor/frontend/src/AddHealthCheckEntryModal/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Modal, Segment } from 'semantic-ui-react'
+import AddHealthCheckEntryForm, { HealthCheckEntryFormValues } from './AddHealthCheckEntryForm'
+
+interface Props {
+  modalOpen: boolean;
+  onClose: () => void;
+  onSubmit: (values: HealthCheckEntryFormValues) => void;
+  error?: string;
+  patientName: string;
+}
+
+const AddHealthCheckEntryModal = ({ modalOpen, onClose, onSubmit, error, patientName }: Props) => (
+  <Modal open={modalOpen} onClose={onClose} centered={false} closeIcon>
+    <Modal.Header>Add Health Check entry for patient: {patientName}</Modal.Header>
+    <Modal.Content>
+      {error && <Segment inverted color="red">{`Error: ${error}`}</Segment>}
+      <AddHealthCheckEntryForm onSubmit={onSubmit} onCancel={onClose} />
+    </Modal.Content>
+  </Modal>
+)
+
+export default AddHealthCheckEntryModal
